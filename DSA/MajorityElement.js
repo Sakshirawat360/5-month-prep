8dda8190aa73cc11371e8a78d666fc2a5d8560c8
Sakshi <youremail@example.com>
2026-03-10 11:32:39 +0530

    Majority element

---
+++ DSA/MajorityElement.js
@@ -0,0 +1,18 @@
+function majority(nums){
+    const map = {}
+
+    for(let num of nums){
+        if(map[num]){
+            map[num]++
+        }
+        else{
+            map[num]=1
+        }
+
+        if(map[num]> nums.length/2){
+            return num
+        }
+    }
+    return "no majority element"
+}
+console.log(majority([1,1,1,1,2,3,3]));
