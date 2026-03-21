eb37ac8cc49a8f2382aed60866656a8b77cda140
Sakshi <youremail@example.com>
2026-03-21 14:34:27 +0530

    promisechaining

---
+++ Promisechain.js
@@ -0,0 +1,32 @@
+// promise chaining - > iska matlb hota hai ,, 1 promise run hone ke baad dusra promise run hota hai,, isko promise chaining bolte hain. 
+// example - API1 -> API2 -> API3
+
+
+function api1(){
+ return new Promise(resolve =>{
+    setTimeout(() => {
+        console.log("API1 DONE");
+        resolve()
+    }, 1000);
+ });
+}
+
+function api2(){
+ return new Promise(resolve =>{
+    setTimeout(() => {
+        console.log("API2 DONE");
+        resolve()
+    }, 1000);
+ });
+}
+
+function api3(){
+ return new Promise(resolve =>{
+    setTimeout(() => {
+        console.log("API3 DONE");
+        resolve()
+    }, 1000);
+ });
+}
+
+api1().then(api2).then(api3)
